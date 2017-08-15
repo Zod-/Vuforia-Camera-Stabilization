507d999ef589deae7b0cf2036d4a38ba9bd20ef5
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2017-08-15 11:16:36 +0200

    Add stabilization status UI

---
+++ Assets/Scripts/StabilizationStatus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class StabilizationStatus : MonoBehaviour
+{
+    [SerializeField]
+    private MonoBehaviour _stabilizationBehaviour;
+    [SerializeField]
+    private GameObject _onText;
+    [SerializeField]
+    private GameObject _offText;
+
+
+    private void Update()
+    {
+        if (_stabilizationBehaviour == null) { return; }
+
+        _onText.SetActive(_stabilizationBehaviour.enabled);
+        _offText.SetActive(!_stabilizationBehaviour.enabled);
+    }
+}
