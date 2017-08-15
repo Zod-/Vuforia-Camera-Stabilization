b96266b38381a9f8017fcd22108e6cb259ef8d05
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2017-08-15 11:15:31 +0200

    Add basic stabilization by Lerping old values

---
+++ Assets/Scripts/CameraStabilizationUsingCallback.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class CameraStabilizationUsingCallback : MonoBehaviour
+{
+    private Vector3 _lastPosition;
+    private Quaternion _lastRotation;
+
+    private void OnEnable()
+    {
+        _lastPosition = transform.localPosition;
+        _lastRotation = transform.localRotation;
+        VuforiaARController.Instance.RegisterTrackablesUpdatedCallback(OnTrackablesUpdated);
+    }
+
+    private void OnDisable()
+    {
+        VuforiaARController.Instance.UnregisterTrackablesUpdatedCallback(OnTrackablesUpdated);
+    }
+
+    private void OnTrackablesUpdated()
+    {
+        transform.localPosition = Vector3.Lerp(_lastPosition, transform.localPosition, Time.deltaTime);
+        transform.localRotation = Quaternion.Lerp(_lastRotation, transform.localRotation, Time.deltaTime);
+
+        _lastPosition = transform.localPosition;
+        _lastRotation = transform.localRotation;
+    }
+}
